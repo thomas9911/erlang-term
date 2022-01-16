b5cd3b735ffbd580fb74711450003bfe20c8fec5
Thomas <thomastimmer11@hotmail.com>
2022-01-16 16:56:10 +0100

    chore: clean up

--- rustler_test/lib/rustler_test.ex
+++ rustler_test/lib/rustler_test.ex
@@ -2,7 +2,6 @@ defmodule RustlerTest do
   use Rustler, otp_app: :rustler_test, crate: "rustlertest"
 
   # When your NIF is loaded, it will override this function.
-  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
   def raw_decode(_a), do: :erlang.nif_error(:nif_not_loaded)
   def decode(_a), do: :erlang.nif_error(:nif_not_loaded)
 end
--- rustler_test/native/rustlertest/src/lib.rs
+++ rustler_test/native/rustlertest/src/lib.rs
@@ -3,11 +3,6 @@ use erlang_term::RawTerm;
 use erlang_term::Term as OwnedTerm;
 
 #[rustler::nif]
-fn add(a: i64, b: i64) -> i64 {
-    a + b
-}
-
-#[rustler::nif]
 fn raw_decode<'a>(term: Term<'a>) -> String {
     let raw_term = term.decode::<RawTerm>();
 
